804eb0eabc8d80901957da632eb90f88163b277d
Emeka Ndubisi Raphael <e84305251@china.huawei.com>
2023-11-16 16:22:08 +0100

    update on package  v1

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// index.js
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
