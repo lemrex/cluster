022758798fbbb12f9bd7528e7e8a3697d1b93f34
Raphael <95745431+lemrex@users.noreply.github.com>
2024-01-05 10:35:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World. Ths is test edit 2024!');
+  res.send('Hello, World. Ths is test edit 2023!');
 });
 
 app.listen(port, () => {
